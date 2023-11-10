556c1e472ecec196fa7f208aca506587a97cbf08
GeorgyFirsov <gfirsov007@gmail.com>
2023-11-10 00:18:56 +0000

    chore: interface generator
    Signed-off-by: GeorgyFirsov <gfirsov007@gmail.com>

--- include/common/interface.h
+++ include/common/interface.h
@@ -44,48 +44,77 @@ typedef struct tagKEY
 
 
 /**
- * @brief Block cipher dispatch table.
+ * @brief Block encryption procedure.
+ *
+ * @param in Plaintext block
+ * @param round_keys Initialized key schedule for encryption
+ * @param out Ciphertext block
  */
-typedef struct tagBLOCK_CIPHER
-{
-    unsigned char block_size; /**< Block size in bytes */
-    unsigned char key_size;   /**< Key size in bytes */
+#define BCLIB_ENCRYPT_BLOCK(block_type)          \
+    void (*encrypt_block)(const block_type in,   \
+                          const KEY* round_keys, \
+                          block_type* out)
 
-    /**
-	 * @brief Block encryption procedure.
-     *
-     * @param in Plaintext block
-     * @param round_keys Initialized key schedule for encryption
-     * @param out Ciphertext block
-	 */
-    void (*encrypt_block)(const __m128i in, const KEY* round_keys, __m128i* out);
 
-    /**
-	 * @brief Block decryption procedure.
-     *
-     * @param in Ciphertext block
-     * @param round_keys Initialized key schedule for decryption
-     * @param out Plaintext block
-	 */
-    void (*decrypt_block)(const __m128i in, const KEY* round_keys, __m128i* out);
+/**
+ * @brief Block decryption procedure.
+ *
+ * @param in Ciphertext block
+ * @param round_keys Initialized key schedule for decryption
+ * @param out Plaintext block
+ */
+#define BCLIB_DECRYPT_BLOCK(block_type)          \
+    void (*decrypt_block)(const block_type in,   \
+                          const KEY* round_keys, \
+                          block_type* out)
 
-    /**
-	 * @brief Encryption key schedule initialization procedure.
-     *
-     * @param key Binary key representation
-     * @param round_keys Key schedule for encryption to initialize
-	 */
-    void (*initialize_encrypt_key)(const unsigned char* key, KEY* round_keys);
 
-    /**
-	 * @brief Decryption key schedule initialization procedure.
-     * 
-     * @param key Binary key representation
-     * @param round_keys Key schedule for decryption to initialize
-	 */
-    void (*initialize_decrypt_key)(const unsigned char* key, KEY* round_keys);
-} BLOCK_CIPHER;
+/**
+ * @brief Encryption key schedule initialization procedure.
+ *
+ * @param key Binary key representation
+ * @param round_keys Key schedule for encryption to initialize
+ */
+#define BCLIB_INIT_ENCRYPT_KEY()                             \
+    void (*initialize_encrypt_key)(const unsigned char* key, \
+                                   KEY* round_keys)
 
+
+/**
+ * @brief Decryption key schedule initialization procedure.
+ * 
+ * @param key Binary key representation
+ * @param round_keys Key schedule for decryption to initialize
+ */
+#define BCLIB_INIT_DECRYPT_KEY()                             \
+    void (*initialize_decrypt_key)(const unsigned char* key, \
+                                   KEY* round_keys)
+
+
+/**
+ * @brief Defines a block cipher dispatch table.
+ * 
+ * Block ciphers with different block sizes are described using different
+ * dispatch tables intentionally to allow implementing algorithms with 
+ * high performance for research purposes.
+ */
+#define BCLIB_DEFINE_CIPHER_TABLE(name, block_type)                                               \
+    typedef struct tag##name                                                                      \
+    {                                                                                             \
+        unsigned char block_size; /**< Block size in bytes */                                     \
+        unsigned char key_size;   /**< Key size in bytes */                                       \
+                                                                                                  \
+        BCLIB_ENCRYPT_BLOCK(block_type); /**< Block encryption procedure */                       \
+        BCLIB_DECRYPT_BLOCK(block_type); /**< Block decryption procedure */                       \
+        BCLIB_INIT_ENCRYPT_KEY();        /**< Encryption key schedule initialization procedure */ \
+        BCLIB_INIT_DECRYPT_KEY();        /**< Decryption key schedule initialization procedure */ \
+    } name
+
+
+/**
+ * @brief 128-bit block cipher dispatch table.
+ */
+BCLIB_DEFINE_CIPHER_TABLE(BLOCK_CIPHER, __m128i);
 
 #ifdef __cplusplus
 }
